5d839fafad4884a83a2a5f2b83c32be6edd18708
Mark Parnell <markparnell@brownbox.net.au>
2016-01-29 10:07:15 +1100

    Moved logo out of menu block

--- sections/nav.php
+++ sections/nav.php
@@ -7,12 +7,10 @@ $small_logo = get_theme_mod(ns_.'logo_small');
     </nav>
     <nav class="top-bar" id="top_menu">
         <section class="top-bar-left hide-for-small">
-            <ul class="menu">
 <?php
 $logo = get_theme_mod(ns_.'logo_large');
 ?>
-                <li class="menu-text"><a href="/"><img id="logo" src="<?php echo $logo; ?>" alt=""></a></li>
-            </ul>
+            <a href="/"><img id="logo" src="<?php echo $logo; ?>" alt=""></a>
         </section>
         <section class="top-bar-right">
             <ul class="menu">
